68e267bd4546cd77b4f08215a642518f32ff2720
James Wilson <james@wayfound.ai>
2024-12-05 10:58:43 -0500

    Update TypeScript declaration file

--- src/recording.d.ts
+++ src/recording.d.ts
@@ -3,7 +3,8 @@ declare module 'recording' {
         wayfoundApiKey?: string;
         agentId?: string;
         recordingId?: string | null;
-        initialMessages?: any[];
+        initialMessages?: Message[];
+        published?: boolean;
     }
 
     export interface Message {
@@ -26,15 +27,27 @@ declare module 'recording' {
         wayfoundApiKey: string;
         agentId: string;
         recordingId: string | null;
+        published: boolean;
         headers: {
             "Content-Type": string;
             "Authorization": string;
+            "X-SDK-Language": string;
+            "X-SDK-Version": string;
         };
 
         constructor(params: RecordingConstructorParams);
 
-        recordMessages(messages: Message[]): void;
+        newRecording(params: { initialMessages?: Message[] }): Promise<string>;
 
-        recordMessagesFromLangchainMemory(memory: LangchainMemory): void;
+        recordMessages(messages: Message[]): Promise<void>;
+
+        completedRecording(params: {
+            messages?: Message[];
+            firstMessageAt?: string | null;
+            lastMessageAt?: string | null;
+            visitorId?: string | null;
+        }): Promise<void>;
+
+        recordMessagesFromLangchainMemory(memory: LangchainMemory): Promise<void>;
     }
 }
