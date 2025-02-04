2c54bd2fc41152bc88bebab1532eb31bcba07fae
James Wilson <james@wayfound.ai>
2025-02-04 15:38:22 -0800

    Added commonjs and esmodule builds

--- src/recording.js
+++ src/recording.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const WAYFOUND_HOST = `https://app.wayfound.ai`;
+const WAYFOUND_HOST = `http://localhost:3000`;
 const WAYFOUND_RECORDING_ACTIVE_URL = `${WAYFOUND_HOST}/api/v1/recordings/active`;
 const WAYFOUND_RECORDING_COMPLETED_URL = `${WAYFOUND_HOST}/api/v1/recordings/completed`;
 const SDK_LANGUAGE = "JavaScript";
